a86c51ffe68b20041f5dc2a99c6e470902e35e5f
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2023-08-07 17:33:23 +0100

    Fixes in Max-Min

--- utils/max-min.js
+++ utils/max-min.js
@@ -1,39 +1,40 @@
 import { readingStore } from "../models/reading-store.js";
-import { stationStore } from "../models/station-store.js";
 
 export const maxMin = {
 
-async maxValue(station, property) {
-    const readings = await readingStore.getReadingsByStationId(station._id);
-    if (readings.length < 1){
-        return "No readings available";
-    } else {
-        let maxVal = readings[0][property];
-    for (let i = 0; i < readings.length; i++) {
-        const value = readings[i][property];
-        if (value > maxVal) {
-            maxVal = value;
+    async maxValue(station, property) {
+        const readings = await readingStore.getReadingsByStationId(station._id);
+        if (readings.length < 2) {
+            return "Not enough data";
+        } else {
+            let maxVal = readings[0][property];
+            for (let i = 0; i < readings.length; i++) {
+                const value = readings[i][property];
+                if (value > maxVal) {
+                    maxVal = value;
+                }
+            }
+            return maxVal;
         }
-    }
-    }
-    
-    return maxVal;
-},
 
-async minValue(station, property) {
-    const readings = await readingStore.getReadingsByStationId(station._id); 
-    if (readings.length < 1){
-        return "No readings available";
-    } else {
-    let minVal = readings[0][property];
-    for (let i = 0; i < readings.length; i++) {
-        const value = readings[i][property];
-        if (value < minVal) {
-            minVal = value;
+        
+    },
+
+    async minValue(station, property) {
+        const readings = await readingStore.getReadingsByStationId(station._id);
+        if (readings.length < 2) {
+            return "Not enough data";
+        } else {
+            let minVal = readings[0][property];
+            for (let i = 0; i < readings.length; i++) {
+                const value = readings[i][property];
+                if (value < minVal) {
+                    minVal = value;
+                }
+            }
+            return minVal;
         }
-    }
-}
-    return minVal;
-},
-    
+        
+    },
+
 };
