30f3107513ee3b3f3dd15ebb47178cfc3e0c9100
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2023-08-13 21:09:53 +0100

    Updated weather codes to match openweather API weather codes, dropdown menu in addreadings

--- utils/analytics.js
+++ utils/analytics.js
@@ -18,9 +18,9 @@ async getLatestReading(stationId) {
     return reading === "No Readings available";
   },
 
-
 celciusToFahrenheit(temperature) {
-    return ((temperature * 9 / 5) + 32);
+  let num = ((temperature * 9 / 5) + 32);
+    return num.toFixed(2);
 },
 
 beaufort(windspeed){
@@ -135,27 +135,9 @@ celciusToIcon(temperature) {
       return "fa-solid fa-temperature-half";
 },
 
+
 codeToText(code) {
-    switch (code) {
-        case 100:
-          return "Clear";
-        case 200:
-          return "Partial Clouds";
-        case 300:
-          return "Cloudy";
-        case 400:
-          return "Light Showers";
-        case 500:
-          return "Heavy Showers";
-        case 600:
-          return "Rain";
-        case 700:
-          return "Snow";
-        case 800:
-          return "Thunder";
-        default:
-          return "Sunny";
-      };
+    return weatherDescriptions[code] || "Unknown";
 },
 
 getIdFromList(list){
@@ -193,3 +175,61 @@ const dateFormatter = new Intl.DateTimeFormat('nl-BE',{
     second: 'numeric',
     hour12: false,
 });
+
+const weatherDescriptions = {
+  200: "Thunderstorm with light rain",
+  201: "Thunderstorm with rain",
+  202: "Thunderstorm with heavy rain",
+  210: "Light thunderstorm",
+  211: "Thunderstorm",
+  212: "Heavy thunderstorm",
+  221: "Ragged thunderstorm",
+  230: "Thunderstorm with light drizzle",
+  231: "Thunderstorm with drizzle",
+  232: "Thunderstorm with heavy drizzle",
+  300: "Light intensity drizzle",
+  301: "Drizzle",
+  302: "Heavy intensity drizzle",
+  310: "Light intensity drizzle rain",
+  311: "Drizzle rain",
+  312: "Heavy intensity drizzle rain",
+  313: "Shower rain and drizzle",
+  314: "Heavy shower rain and drizzle",
+  321: "Shower drizzle",
+  500: "Light rain",
+  501: "Moderate rain",
+  502: "Heavy intensity rain",
+  503: "Very heavy rain",
+  504: "Extreme rain",
+  511: "Freezing rain",
+  520: "Light intensity shower rain",
+  521: "Shower rain",
+  522: "Heavy intensity shower rain",
+  531: "Ragged shower rain",
+  600: "Light snow",
+  601: "Snow",
+  602: "Heavy snow",
+  611: "Sleet",
+  612: "Light shower sleet",
+  613: "Shower sleet",
+  615: "Light rain and snow",
+  616: "Rain and snow",
+  620: "Light shower snow",
+  621: "Shower snow",
+  622: "Heavy shower snow",
+  701: "Mist",
+  711: "Smoke",
+  721: "Haze",
+  731: "Sand/dust whirls",
+  741: "Fog",
+  751: "Sand",
+  761: "Dust",
+  762: "Volcanic ash",
+  771: "Squalls",
+  781: "Tornado",
+  800: "Clear sky",
+  801: "Few clouds: 11-25%",
+  802: "Scattered clouds: 25-50%",
+  803: "Broken clouds: 51-84%",
+  804: "Overcast clouds: 85-100%",
+};
